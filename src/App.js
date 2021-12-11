09c41024de14abefa7fa74f91a58951d6dae6b20
Heber Lazo <heberlazo@gmail.com>
2021-12-11 15:34:18 -0500

    Added navbar links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Cart from "./components/Cart";
 import store from "./store/configureStore";
 import { loadProducts } from "./store/products";
 
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Provider>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,17 +3,26 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <Link to="/">
-          <li>Casa</li>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          Home
         </Link>
-      </ul>
-      <ul>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-      </ul>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/cart" className="nav-link">
+                Cart
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
