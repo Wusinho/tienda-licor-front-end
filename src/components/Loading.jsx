3f7544886db6b8e7a46c38c4e6e43fe8b75537eb
Heber Lazo <heberlazo@gmail.com>
2021-12-12 19:14:47 -0500

    Added error handling if not loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import "../style/Loading.css";
 
 const Loading = () => (
-  <div className="lds-ripple">
-    <div />
-    <div />
+  <div className=" d-flex aligns-items-center justify-content-center">
+    <div className=" ">
+      <h2>Oopz! Something went wrong. Please come back in a few minutes</h2>
+      <small>
+        or conctact:
+        <a href="mailto: help@helpdesk.com">help@helpdesk.com</a>
+      </small>
+      <div className="bar "></div>
+    </div>
   </div>
 );
 
